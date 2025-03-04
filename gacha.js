a721b605d8096a167327cc6b28d571d3e8d7dcd4
bura_ryokumari <buranco828@gmail.com>
2025-03-04 17:45:25 +0900

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -30,7 +30,7 @@ document.getElementById("gacha-button").addEventListener("click", function() {
     let selectedItems = [];
     let totalPrice = 0;
 
-    while (9000 < totalPrice < budget) {
+    while (9500 ≦ totalPrice < budget) {
         let item = menu[Math.floor(Math.random() * menu.length)];
         if (totalPrice + item.price > budget) break;
         selectedItems.push(item);
